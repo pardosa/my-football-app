17996ef123888dae21a8bb5b19af49fe76fc9d7d
Harry <harry@mediainfogrup.com>
2023-02-15 23:51:26 +0700

    remove old table and display leagues data using schematable components

--- src/pages/leagues/index.tsx
+++ src/pages/leagues/index.tsx
@@ -1,257 +1,93 @@
-import React, { useEffect } from "react";
+import { useEffect, useMemo } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { alpha } from "@mui/material/styles";
 import Box from "@mui/material/Box";
-import Table from "@mui/material/Table";
-import TableBody from "@mui/material/TableBody";
-import TableCell from "@mui/material/TableCell";
-import TableContainer from "@mui/material/TableContainer";
-import TableHead from "@mui/material/TableHead";
-import TablePagination from "@mui/material/TablePagination";
-import TableRow from "@mui/material/TableRow";
-import Toolbar from "@mui/material/Toolbar";
-import Paper from "@mui/material/Paper";
-import FormControlLabel from "@mui/material/FormControlLabel";
-import Switch from "@mui/material/Switch";
-import Avatar from "@mui/material/Avatar";
 import CircularProgress from "@mui/material/CircularProgress";
-import Stack from "@mui/material/Stack";
-import ListItemText from "@mui/material/ListItemText";
-import ListItemAvatar from "@mui/material/ListItemAvatar";
 
-import { AppDispatch } from "../../store";
-import {
+import { AppDispatch, dispatch } from "../../store";
+import leagues, {
   getLeagues,
   getLeaguesStatus,
   selectAllLeagues,
 } from "../../store/reducers/leagues";
-import { Country, TLeague, TLeagues } from "../../store/types";
+import SchemaTable, { IColumnConfig } from "../../components/SchemaTable";
+import { components } from "../../types/openapi";
+import openapi from "../../openapi.json";
+import { SchemaObject } from "openapi3-ts";
+import { Stack, ListItemAvatar, Avatar, ListItemText } from "@mui/material";
 
-interface HeadCell {
-  disablePadding: boolean;
-  id: keyof TLeague | keyof Country;
-  label: string;
-  numeric: boolean;
-}
-
-const headCells: readonly HeadCell[] = [
-  {
-    id: "code",
-    numeric: false,
-    disablePadding: true,
-    label: "Country",
-  },
-  {
-    id: "name",
-    numeric: false,
-    disablePadding: true,
-    label: "Name",
-  },
-  {
-    id: "type",
-    numeric: false,
-    disablePadding: true,
-    label: "Type",
-  },
-];
-
-// interface EnhancedTableProps {
-//   numSelected: number;
-//   onRequestSort: (
-//     event: React.MouseEvent<unknown>,
-//     property: keyof TLeague | keyof Country
-//   ) => void;
-//   onSelectAllClick: (event: React.ChangeEvent<HTMLInputElement>) => void;
-//   order: Order;
-//   orderBy: string;
-//   rowCount: number;
-// }
-
-interface EnhancedTableToolbarProps {
-  numSelected: number;
-}
-
-function EnhancedTableHead() {
-  return (
-    <TableHead>
-      <TableRow>
-        {headCells.map((headCell) => (
-          <TableCell
-            key={headCell.id}
-            align={headCell.numeric ? "right" : "left"}
-            padding={headCell.disablePadding ? "none" : "normal"}
-            sortDirection={false}
-          >
-            {headCell.label.toUpperCase()}
-          </TableCell>
-        ))}
-      </TableRow>
-    </TableHead>
-  );
-}
-
-function EnhancedTableToolbar() {
-  return (
-    <Toolbar
-      sx={{
-        pl: { sm: 2 },
-        pr: { xs: 1, sm: 1 },
-      }}
-    >
-      {
-        //Search toolbar
-      }
-    </Toolbar>
-  );
-}
+type TLeagues = components["schemas"]["Leagues"];
 
 export default function Leagues() {
   const dispatch = useDispatch<AppDispatch>();
   const leagues: TLeagues[] = useSelector(selectAllLeagues);
   const status = useSelector(getLeaguesStatus);
   //const error = useSelector(getLeaguesError);
+  const leaguesSchema = useMemo(
+    () =>
+      ({
+        properties: {
+          ...openapi.components.schemas.Leagues.properties,
+        },
+      } as SchemaObject),
+    []
+  );
 
-  const [selected, setSelected] = React.useState<readonly string[]>([]);
-  const [page, setPage] = React.useState(0);
-  const [dense, setDense] = React.useState(false);
-  const [rowsPerPage, setRowsPerPage] = React.useState(5);
-
-  const handleClick = (event: React.MouseEvent<unknown>, name: string) => {
-    // const selectedIndex = selected.indexOf(name);
-    // let newSelected: readonly string[] = [];
-    // if (selectedIndex === -1) {
-    //   newSelected = newSelected.concat(selected, name);
-    // } else if (selectedIndex === 0) {
-    //   newSelected = newSelected.concat(selected.slice(1));
-    // } else if (selectedIndex === selected.length - 1) {
-    //   newSelected = newSelected.concat(selected.slice(0, -1));
-    // } else if (selectedIndex > 0) {
-    //   newSelected = newSelected.concat(
-    //     selected.slice(0, selectedIndex),
-    //     selected.slice(selectedIndex + 1)
-    //   );
-    // }
-    // setSelected(newSelected);
-  };
-
-  const handleChangePage = (event: unknown, newPage: number) => {
-    setPage(newPage);
-  };
-
-  const handleChangeRowsPerPage = (
-    event: React.ChangeEvent<HTMLInputElement>
-  ) => {
-    setRowsPerPage(parseInt(event.target.value, 10));
-    setPage(0);
-  };
-
-  const handleChangeDense = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setDense(event.target.checked);
-  };
-
-  const isSelected = (name: string) => selected.indexOf(name) !== -1;
-
-  const emptyRows =
-    page > 0 ? Math.max(0, (1 + page) * rowsPerPage - leagues.length) : 0;
+  const config = useMemo<{
+    [propName: string]: IColumnConfig<components["schemas"]["Leagues"]>;
+  }>(
+    () => ({
+      league: {
+        title: "League Name",
+        renderCell: (row) => {
+          return row.league?.logo !== null ? (
+            <Stack direction="row" spacing={4}>
+              <ListItemAvatar>
+                <Avatar alt={row.league?.name} src={row.league?.logo} />
+              </ListItemAvatar>
+              <ListItemText
+                primary={row.league?.name}
+                secondary={row.seasons?.length + " seasons"}
+              />
+            </Stack>
+          ) : null;
+        },
+      },
+      seasons: {
+        title: "Type",
+        renderCell: (row) => <>{row.league?.type}</>,
+      },
+      country: {
+        title: "Country",
+        renderCell: (row) => {
+          return row.country?.flag !== null ? (
+            <Stack direction="row" spacing={4}>
+              <ListItemAvatar>
+                <Avatar alt={row.country?.name} src={row.country?.flag} />
+              </ListItemAvatar>
+              <ListItemText primary={row.country?.name} secondary={""} />
+            </Stack>
+          ) : null;
+        },
+      },
+    }),
+    []
+  );
 
   useEffect(() => {
     if (leagues.length === 0) dispatch(getLeagues());
   }, [dispatch, leagues.length]);
 
   return status === "succeeded" && leagues.length > 0 ? (
-    <Box sx={{ width: "100%" }}>
-      <Paper sx={{ width: "100%", mb: 2 }}>
-        <EnhancedTableToolbar />
-        <TableContainer>
-          <Table
-            sx={{ minWidth: 750 }}
-            aria-labelledby="tableTitle"
-            size={dense ? "small" : "medium"}
-          >
-            <EnhancedTableHead />
-            <TableBody>
-              {leagues
-                .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-                .map((row, index) => {
-                  const isItemSelected = isSelected(row.league.name);
-                  const labelId = `enhanced-table-checkbox-${index}`;
-
-                  return (
-                    <TableRow
-                      hover
-                      onClick={(event) => handleClick(event, row.league.name)}
-                      tabIndex={-1}
-                      key={row.league.id}
-                      selected={isItemSelected}
-                    >
-                      <TableCell align="left" padding="none">
-                        {row.country.flag !== null ? (
-                          <Stack direction="row" spacing={4}>
-                            <ListItemAvatar>
-                              <Avatar
-                                alt={row.country.name}
-                                src={row.country.flag}
-                              />
-                            </ListItemAvatar>
-                            <ListItemText
-                              primary={row.country.name}
-                              secondary={""}
-                            />
-                          </Stack>
-                        ) : null}
-                      </TableCell>
-                      <TableCell
-                        component="th"
-                        id={labelId}
-                        scope="row"
-                        padding="none"
-                      >
-                        {row.league.logo !== null ? (
-                          <Stack direction="row" spacing={4}>
-                            <ListItemAvatar>
-                              <Avatar
-                                alt={row.league.name}
-                                src={row.league.logo}
-                              />
-                            </ListItemAvatar>
-                            <ListItemText
-                              primary={row.league.name}
-                              secondary={row.seasons.length + " seasons"}
-                            />
-                          </Stack>
-                        ) : null}
-                      </TableCell>
-                      <TableCell align="left">{row.league.type}</TableCell>
-                    </TableRow>
-                  );
-                })}
-              {emptyRows > 0 && (
-                <TableRow
-                  style={{
-                    height: (dense ? 33 : 53) * emptyRows,
-                  }}
-                >
-                  <TableCell colSpan={6} />
-                </TableRow>
-              )}
-            </TableBody>
-          </Table>
-        </TableContainer>
-        <TablePagination
-          rowsPerPageOptions={[5, 10, 25]}
-          component="div"
-          count={leagues.length}
-          rowsPerPage={rowsPerPage}
-          page={page}
-          onPageChange={handleChangePage}
-          onRowsPerPageChange={handleChangeRowsPerPage}
-        />
-      </Paper>
-      <FormControlLabel
-        control={<Switch checked={dense} onChange={handleChangeDense} />}
-        label="Dense padding"
-      />
-    </Box>
+    <SchemaTable<TLeagues>
+      data={leagues}
+      schema={leaguesSchema}
+      height={window.innerHeight}
+      width={window.innerWidth - 25}
+      config={config}
+      onRowClick={(row) => {
+        console.log(row);
+      }}
+    />
   ) : (
     <Box sx={{ display: "flex" }}>
       <CircularProgress />
