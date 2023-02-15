fca7c06c8fe2e511dd53c72156e49924ceb96a1a
Harry <harry@mediainfogrup.com>
2023-02-15 12:05:31 +0700

    change table column

--- src/pages/leagues/index.tsx
+++ src/pages/leagues/index.tsx
@@ -9,13 +9,8 @@ import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TablePagination from "@mui/material/TablePagination";
 import TableRow from "@mui/material/TableRow";
-import TableSortLabel from "@mui/material/TableSortLabel";
 import Toolbar from "@mui/material/Toolbar";
-import Typography from "@mui/material/Typography";
 import Paper from "@mui/material/Paper";
-import Checkbox from "@mui/material/Checkbox";
-import IconButton from "@mui/material/IconButton";
-import Tooltip from "@mui/material/Tooltip";
 import FormControlLabel from "@mui/material/FormControlLabel";
 import Switch from "@mui/material/Switch";
 import Avatar from "@mui/material/Avatar";
@@ -23,55 +18,14 @@ import CircularProgress from "@mui/material/CircularProgress";
 import Stack from "@mui/material/Stack";
 import ListItemText from "@mui/material/ListItemText";
 import ListItemAvatar from "@mui/material/ListItemAvatar";
-import { visuallyHidden } from "@mui/utils";
 
 import { AppDispatch } from "../../store";
 import {
   getLeagues,
-  getLeaguesError,
   getLeaguesStatus,
   selectAllLeagues,
 } from "../../store/reducers/leagues";
 import { Country, TLeague, TLeagues } from "../../store/types";
-
-function descendingComparator<T>(a: T, b: T, orderBy: keyof T) {
-  if (b[orderBy] < a[orderBy]) {
-    return -1;
-  }
-  if (b[orderBy] > a[orderBy]) {
-    return 1;
-  }
-  return 0;
-}
-
-type Order = "asc" | "desc";
-
-function getComparator<Key extends keyof any>(
-  order: Order,
-  orderBy: Key
-): (
-  a: { [key in Key]: number | string },
-  b: { [key in Key]: number | string }
-) => number {
-  return order === "desc"
-    ? (a, b) => descendingComparator(a, b, orderBy)
-    : (a, b) => -descendingComparator(a, b, orderBy);
-}
-
-function stableSort<T>(
-  array: readonly T[],
-  comparator: (a: T, b: T) => number
-) {
-  const stabilizedThis = array.map((el, index) => [el, index] as [T, number]);
-  stabilizedThis.sort((a, b) => {
-    const order = comparator(a[0], b[0]);
-    if (order !== 0) {
-      return order;
-    }
-    return a[1] - b[1];
-  });
-  return stabilizedThis.map((el) => el[0]);
-}
 
 interface HeadCell {
   disablePadding: boolean;
@@ -81,6 +35,12 @@ interface HeadCell {
 }
 
 const headCells: readonly HeadCell[] = [
+  {
+    id: "code",
+    numeric: false,
+    disablePadding: true,
+    label: "Country",
+  },
   {
     id: "name",
     numeric: false,
@@ -93,84 +53,36 @@ const headCells: readonly HeadCell[] = [
     disablePadding: true,
     label: "Type",
   },
-  {
-    id: "logo",
-    numeric: false,
-    disablePadding: true,
-    label: "Logo",
-  },
-  {
-    id: "code",
-    numeric: false,
-    disablePadding: true,
-    label: "Country",
-  },
 ];
 
-interface EnhancedTableProps {
-  numSelected: number;
-  onRequestSort: (
-    event: React.MouseEvent<unknown>,
-    property: keyof TLeague | keyof Country
-  ) => void;
-  onSelectAllClick: (event: React.ChangeEvent<HTMLInputElement>) => void;
-  order: Order;
-  orderBy: string;
-  rowCount: number;
-}
+// interface EnhancedTableProps {
+//   numSelected: number;
+//   onRequestSort: (
+//     event: React.MouseEvent<unknown>,
+//     property: keyof TLeague | keyof Country
+//   ) => void;
+//   onSelectAllClick: (event: React.ChangeEvent<HTMLInputElement>) => void;
+//   order: Order;
+//   orderBy: string;
+//   rowCount: number;
+// }
 
 interface EnhancedTableToolbarProps {
   numSelected: number;
 }
 
-function EnhancedTableHead(props: EnhancedTableProps) {
-  const {
-    onSelectAllClick,
-    order,
-    orderBy,
-    numSelected,
-    rowCount,
-    onRequestSort,
-  } = props;
-  const createSortHandler =
-    (property: keyof TLeague | keyof Country) =>
-    (event: React.MouseEvent<unknown>) => {
-      onRequestSort(event, property);
-    };
-
+function EnhancedTableHead() {
   return (
     <TableHead>
       <TableRow>
-        <TableCell padding="checkbox">
-          <Checkbox
-            color="primary"
-            indeterminate={numSelected > 0 && numSelected < rowCount}
-            checked={rowCount > 0 && numSelected === rowCount}
-            onChange={onSelectAllClick}
-            inputProps={{
-              "aria-label": "select all leagues",
-            }}
-          />
-        </TableCell>
         {headCells.map((headCell) => (
           <TableCell
             key={headCell.id}
             align={headCell.numeric ? "right" : "left"}
             padding={headCell.disablePadding ? "none" : "normal"}
-            sortDirection={orderBy === headCell.id ? order : false}
+            sortDirection={false}
           >
-            <TableSortLabel
-              active={orderBy === headCell.id}
-              direction={orderBy === headCell.id ? order : "asc"}
-              onClick={createSortHandler(headCell.id)}
-            >
-              {headCell.label}
-              {orderBy === headCell.id ? (
-                <Box component="span" sx={visuallyHidden}>
-                  {order === "desc" ? "sorted descending" : "sorted ascending"}
-                </Box>
-              ) : null}
-            </TableSortLabel>
+            {headCell.label.toUpperCase()}
           </TableCell>
         ))}
       </TableRow>
@@ -178,51 +90,17 @@ function EnhancedTableHead(props: EnhancedTableProps) {
   );
 }
 
-function EnhancedTableToolbar(props: EnhancedTableToolbarProps) {
-  const { numSelected } = props;
-
+function EnhancedTableToolbar() {
   return (
     <Toolbar
       sx={{
         pl: { sm: 2 },
         pr: { xs: 1, sm: 1 },
-        ...(numSelected > 0 && {
-          bgcolor: (theme) =>
-            alpha(
-              theme.palette.primary.main,
-              theme.palette.action.activatedOpacity
-            ),
-        }),
       }}
     >
-      {numSelected > 0 ? (
-        <Typography
-          sx={{ flex: "1 1 100%" }}
-          color="inherit"
-          variant="subtitle1"
-          component="div"
-        >
-          {numSelected} selected
-        </Typography>
-      ) : (
-        <Typography
-          sx={{ flex: "1 1 100%" }}
-          variant="h6"
-          id="tableTitle"
-          component="div"
-        >
-          Leagues
-        </Typography>
-      )}
-      {numSelected > 0 ? (
-        <Tooltip title="Delete">
-          <IconButton></IconButton>
-        </Tooltip>
-      ) : (
-        <Tooltip title="Filter list">
-          <IconButton></IconButton>
-        </Tooltip>
-      )}
+      {
+        //Search toolbar
+      }
     </Toolbar>
   );
 }
@@ -231,53 +109,29 @@ export default function Leagues() {
   const dispatch = useDispatch<AppDispatch>();
   const leagues: TLeagues[] = useSelector(selectAllLeagues);
   const status = useSelector(getLeaguesStatus);
-  const error = useSelector(getLeaguesError);
+  //const error = useSelector(getLeaguesError);
 
-  const [order, setOrder] = React.useState<Order>("asc");
-  const [orderBy, setOrderBy] = React.useState<keyof TLeague | keyof Country>(
-    "name"
-  );
   const [selected, setSelected] = React.useState<readonly string[]>([]);
   const [page, setPage] = React.useState(0);
   const [dense, setDense] = React.useState(false);
   const [rowsPerPage, setRowsPerPage] = React.useState(5);
 
-  const handleRequestSort = (
-    event: React.MouseEvent<unknown>,
-    property: keyof TLeague | keyof Country
-  ) => {
-    const isAsc = orderBy === property && order === "asc";
-    setOrder(isAsc ? "desc" : "asc");
-    setOrderBy(property);
-  };
-
-  const handleSelectAllClick = (event: React.ChangeEvent<HTMLInputElement>) => {
-    if (event.target.checked) {
-      const newSelected = leagues.map((n: TLeagues) => n.league.name);
-      setSelected(newSelected);
-      return;
-    }
-    setSelected([]);
-  };
-
   const handleClick = (event: React.MouseEvent<unknown>, name: string) => {
-    const selectedIndex = selected.indexOf(name);
-    let newSelected: readonly string[] = [];
-
-    if (selectedIndex === -1) {
-      newSelected = newSelected.concat(selected, name);
-    } else if (selectedIndex === 0) {
-      newSelected = newSelected.concat(selected.slice(1));
-    } else if (selectedIndex === selected.length - 1) {
-      newSelected = newSelected.concat(selected.slice(0, -1));
-    } else if (selectedIndex > 0) {
-      newSelected = newSelected.concat(
-        selected.slice(0, selectedIndex),
-        selected.slice(selectedIndex + 1)
-      );
-    }
-
-    setSelected(newSelected);
+    // const selectedIndex = selected.indexOf(name);
+    // let newSelected: readonly string[] = [];
+    // if (selectedIndex === -1) {
+    //   newSelected = newSelected.concat(selected, name);
+    // } else if (selectedIndex === 0) {
+    //   newSelected = newSelected.concat(selected.slice(1));
+    // } else if (selectedIndex === selected.length - 1) {
+    //   newSelected = newSelected.concat(selected.slice(0, -1));
+    // } else if (selectedIndex > 0) {
+    //   newSelected = newSelected.concat(
+    //     selected.slice(0, selectedIndex),
+    //     selected.slice(selectedIndex + 1)
+    //   );
+    // }
+    // setSelected(newSelected);
   };
 
   const handleChangePage = (event: unknown, newPage: number) => {
@@ -307,21 +161,14 @@ export default function Leagues() {
   return status === "succeeded" && leagues.length > 0 ? (
     <Box sx={{ width: "100%" }}>
       <Paper sx={{ width: "100%", mb: 2 }}>
-        <EnhancedTableToolbar numSelected={selected.length} />
+        <EnhancedTableToolbar />
         <TableContainer>
           <Table
             sx={{ minWidth: 750 }}
             aria-labelledby="tableTitle"
             size={dense ? "small" : "medium"}
           >
-            <EnhancedTableHead
-              numSelected={selected.length}
-              order={order}
-              orderBy={orderBy}
-              onSelectAllClick={handleSelectAllClick}
-              onRequestSort={handleRequestSort}
-              rowCount={leagues.length}
-            />
+            <EnhancedTableHead />
             <TableBody>
               {leagues
                 .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
@@ -333,20 +180,25 @@ export default function Leagues() {
                     <TableRow
                       hover
                       onClick={(event) => handleClick(event, row.league.name)}
-                      role="checkbox"
-                      aria-checked={isItemSelected}
                       tabIndex={-1}
-                      key={row.league.name}
+                      key={row.league.id}
                       selected={isItemSelected}
                     >
-                      <TableCell padding="checkbox">
-                        <Checkbox
-                          color="primary"
-                          checked={isItemSelected}
-                          inputProps={{
-                            "aria-labelledby": labelId,
-                          }}
-                        />
+                      <TableCell align="left" padding="none">
+                        {row.country.flag !== null ? (
+                          <Stack direction="row" spacing={4}>
+                            <ListItemAvatar>
+                              <Avatar
+                                alt={row.country.name}
+                                src={row.country.flag}
+                              />
+                            </ListItemAvatar>
+                            <ListItemText
+                              primary={row.country.name}
+                              secondary={""}
+                            />
+                          </Stack>
+                        ) : null}
                       </TableCell>
                       <TableCell
                         component="th"
@@ -354,33 +206,22 @@ export default function Leagues() {
                         scope="row"
                         padding="none"
                       >
-                        {row.league.name}
-                      </TableCell>
-                      <TableCell align="left">{row.league.type}</TableCell>
-                      <TableCell align="center">
-                        <Avatar
-                          alt={row.league.name}
-                          src={row.league.logo}
-                          sx={{ width: 34, height: 34 }}
-                        />
-                      </TableCell>
-                      <TableCell align="left">
-                        {row.country.flag !== null ? (
+                        {row.league.logo !== null ? (
                           <Stack direction="row" spacing={4}>
                             <ListItemAvatar>
                               <Avatar
-                                alt={row.country.name}
-                                src={row.country.flag}
-                                sx={{ width: 24, height: 24 }}
+                                alt={row.league.name}
+                                src={row.league.logo}
                               />
                             </ListItemAvatar>
                             <ListItemText
-                              primary={row.country.name}
+                              primary={row.league.name}
                               secondary={row.seasons.length + " seasons"}
                             />
                           </Stack>
                         ) : null}
                       </TableCell>
+                      <TableCell align="left">{row.league.type}</TableCell>
                     </TableRow>
                   );
                 })}
